3bb31ee738271ee3e003690d109ae4e0f8cce59d
MonoliYoda <48124492+MonoliYoda@users.noreply.github.com>
2022-04-03 15:12:57 +0200

    Basic CTA layout

---
+++ src/components/HomeCTA/HomeCTA.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./HomeCTA.scss";
+
+export default function HomeCTA() {
+  return (
+    <section className="cta">
+      <div className="image-container">
+        <div className="image"></div>
+      </div>
+      <div className="action">
+        <p>Zacznij pomagać!</p>
+        <p>Oddaj niechciane rzeczy w zaufane ręce</p>
+      </div>
+    </section>
+  );
+}
