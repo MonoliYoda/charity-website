aef7a9f9114390b3291d857859429ee86b78e65e
MonoliYoda <48124492+MonoliYoda@users.noreply.github.com>
2022-04-10 17:45:06 +0200

    Added scroll navigation for contact section

--- src/components/HomeContact/HomeContact.js
+++ src/components/HomeContact/HomeContact.js
@@ -9,7 +9,7 @@ export default function HomeContact() {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
   return (
     <>
-      <section className="contact">
+      <section className="contact" name="contact">
         <div className="image"></div>
         <div className="right">
           <h1>O nas</h1>
