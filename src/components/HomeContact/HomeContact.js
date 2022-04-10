8969e94bdd6d9e6281b77b23219bc5d978d71211
MonoliYoda <48124492+MonoliYoda@users.noreply.github.com>
2022-04-10 20:23:14 +0200

    Added form validation

--- src/components/HomeContact/HomeContact.js
+++ src/components/HomeContact/HomeContact.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./HomeContact.scss";
 import decoration from "../../assets/Decoration.svg";
 import Facebook from "../../assets/Facebook.svg";
 import Instagram from "../../assets/Instagram.svg";
 
 export default function HomeContact() {
+  const [nameField, setNameField] = useState("");
+  const [emailField, setEmailField] = useState("");
+  const [msgField, setMsgField] = useState("");
+
+  const [nameError, setNameError] = useState(true);
+  const [emailError, setEmailError] = useState(false);
+  const [msgError, setMsgError] = useState(false);
+
+  const validateName = () => {
+    return nameField.match("^[A-Za-z]{1,20}$");
+  };
+
+  const validateEmail = () => {
+    return emailField.match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  };
+
+  const validateMsg = () => {
+    return msgField.length >= 120;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (validateName()) {
+      setNameError(false);
+    } else {
+      setNameError(true);
+    }
+    if (validateEmail()) {
+      setEmailError(false);
+    } else {
+      setEmailError(true);
+    }
+    if (validateMsg()) {
+      setMsgError(false);
+    } else {
+      setMsgError(true);
+    }
+  };
+
   const textareaPlaceholder =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
   return (
@@ -18,23 +59,60 @@ export default function HomeContact() {
             <div className="form-top">
               <div>
                 <label for="name">Wpisz swoje imię</label>
-                <input type="text" id="name" placeholder="Mike" />
+                <input
+                  className={nameError ? "errored" : ""}
+                  type="text"
+                  id="name"
+                  placeholder="Mike"
+                  value={nameField}
+                  onChange={(e) => setNameField(e.target.value)}
+                />
+                <p
+                  className="error-message"
+                  style={nameError ? { display: "block" } : { display: "none" }}
+                >
+                  Podane imię jest nieprawidłowe!
+                </p>
               </div>
               <div>
                 <label for="email">Wpisz swój email</label>
-                <input type="email" id="email" placeholder="mike@tyson.com" />
+                <input
+                  className={emailError ? "errored" : ""}
+                  type="email"
+                  id="email"
+                  placeholder="mike@tyson.com"
+                  value={emailField}
+                  onChange={(e) => setEmailField(e.target.value)}
+                />
+                <p
+                  className="error-message"
+                  style={
+                    emailError ? { display: "block" } : { display: "none" }
+                  }
+                >
+                  Podany email jest nieprawidłowy!
+                </p>
               </div>
             </div>
             <div className="form-middle">
               <label for="message">Wpisz swoją wiadomość</label>
               <textarea
+                className={msgError ? "errored" : ""}
                 id="message"
                 placeholder={textareaPlaceholder}
                 rows="5"
+                value={msgField}
+                onChange={(e) => setMsgField(e.target.value)}
               ></textarea>
+              <p
+                className="error-message"
+                style={msgError ? { display: "block" } : { display: "none" }}
+              >
+                Wiadomość musi mieć conajmniej 120 znaków!
+              </p>
             </div>
             <div className="form-bottom">
-              <button>Wyślij</button>
+              <button onClick={handleSubmit}>Wyślij</button>
             </div>
           </form>
         </div>
